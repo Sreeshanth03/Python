c3547d55640eb256b3f8ce0aab847db3c6da3d98
Sreeshanth03 <sreeshanthsurukutla03@gmail.com>
2025-03-21 15:00:19 +0530

    msg

---
+++ task1.py
@@ -0,0 +1,141 @@
+# iv. 	 Write a program to find the greatest of two numbers
+# n=int(input("enter the first number:"))
+# m=int(input("enter the second number:"))
+# if(m>n):
+#     print("The greatest number is",m)
+# else:
+#     print("The greatest number is",n)
+
+# v.	Print "Pass" if a student scores more than 40 marks;  otherwise, print "Fail." 
+# n=int(input("Enter the Marks:"))
+
+
+# if(n>=40):
+#     print("Pass")
+# elif(n<40):
+#     print("Fail")
+
+# vi.	Write a program to display the day of the week based on a  number input (1 for Monday, 2 for Tuesday, etc.). 
+
+# n=int(input("Enter the Week:"))
+# if(n==1):
+#     print("Monday")
+# elif(n==2):
+#     print("Tuesday")
+# elif(n==3):
+#     print("Wednesday")
+# elif(n==4):
+#     print("Thursday")
+# elif(n==5):
+#     print("Friday")
+# elif(n==6):
+#     print("Saturday")
+# elif(n==7):
+#     print("Sunday")
+
+# **********FizzBuzz********
+# n=int(input("Enter the number:"))
+# if(n%15==0):
+#     print("FizzBuzz")
+# elif(n%3==0):
+#     print("Fizz")
+# elif(n%5==0):
+#     print("Buzz")
+# else:
+#     print("Invalid")
+
+# vii.	Implement a simple calculator to perform addition,  subtraction, multiplication, and division. 
+
+# num1=int(input("Enter The First Number:"))
+# num2=int(input("Enter The Second Number:"))
+# x=input("Enter(+,-,*,/,add,sub,mul,div):").lower()
+# if x in ["add","a","ADD","ad","+"]:
+#     print(num1+num2)
+# elif x in ["-","Sub","a",]:
+#     print(num1-num2)
+# elif x in ["*","mul","MuL"]:
+#     print(num1*num2)
+# elif x in ["/","div","Div"]:
+#     print(num1/num2)
+# else:
+#     print("Invalid")
+
+# viii.	Write a program to display the name of a month based on  the month number (1 for January, 2 for February, etc.). 
+
+# n=int(input("Enter the number:"))
+# if(n==1):
+#     print("Jan")
+# elif(n==2):
+#     print("Feb")
+# elif(n==3):
+#     print("Mar")
+# elif(n==4):
+#     print("April")
+# elif(n==5):
+#     print("May")
+# elif(n==6):
+#     print("June")
+# elif(n==7):
+#     print("July")
+# elif(n==8):
+#     print("August")
+# elif(n==9):
+#     print("Sep")
+# elif(n==10):
+#     print(Oct)
+# elif(n==11):
+#     print("Nov")
+# elif(n==12):
+#     print("Dec")
+# else:
+#     print("Invalid")
+
+# b.  Medium Questions: 
+# i.	Write a program to find the greatest of three numbers. 
+# x=int(input("Enter The First Number:"))
+# y=int(input("Enter The Second Number:"))
+# z=int(input("Enter The Thrid Number:"))
+# if(x>y and x>z):
+#     print("The Greatest Number is",x)
+# elif y > x and y > z:
+#     print("The Greatest Number is", y)
+# elif z > x and z > y:
+#     print("The Greatest Number is", z)
+# else:
+#     print("The input is Invalid")
+
+# ii.	Check if a year is a leap year. 
+# n=int(input("Enter The Year:"))
+# if(n%4==0 and n%100 !=0 and n%400==0):
+#     print("Its is a Leap Year")
+# else:
+#     print("Its not a leap year")
+
+# iii.	Write a program to classify a character entered by the user  as a vowel, consonant, or neither. 
+
+# n=input('Enter:').lower()
+# if n in ["a","e","i","o","u"]:
+#     print("It is a  Vowels")
+# else:
+#     print("It does not vowels")
+
+# iv.	Calculate the grade of a student based on the marks they  score: 
+# 1.	90-100  : Grade A 
+# 2.	80-89  : Grade B 
+# 3.	70-79  : Grade C 
+# 4.	<70  : Fail. 
+
+
+#  v. 	 Write a program to check if three sides length form a valid  triangle. 
+
+# x=int(input("Enter the x:"))
+# y=int(input("Enter the y:"))
+# z=int(input("Enter the z:"))
+# if(x+y>z and y+z>x and x+z>y):
+#     print("The Triangle is possible")
+# else:
+#     print("Invalid Triangle")
+
+
+
+

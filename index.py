8b3cf230e8b786eff3f47cad27662a77027415be
Sreeshanth03 <sreeshanthsurukutla03@gmail.com>
2025-03-26 18:58:17 +0530

    msg

--- index.py
+++ index.py
@@ -135,6 +135,139 @@
 #     print("The Triangle is possible")
 # else:
 #     print("Invalid Triangle")
+# for i in range(0,100):
+#     printI(i)
+
+
+# # ii.	Write a program to print the sum of the first  n  natural  numbers. (n*n+1/ 2) 
+# n=int(input("Enter the following:"))
+# sum=(n * (n + 1)) // 2
+# print(f"Sum of the first {n} natural numbers is: {sum_natural}")
+
+
+# sum=0;
+# i=1;
+# while i<50:
+#     if(i%2==0):
+
+#         sum+=2
+#     i+=1
+# print(sum)
+
+
+# n=int(input("Enter the number:"))
+# for i in range(0,21):
+#             print(i*n)
+
+
+# v.	Reverse a number using a  	 while  loop. 
+#  1.  Also can we get the sum of all the digits.  
+
+
+# i=10;
+# sum=0
+# for i in range(10,0,-1):
+#     sum+=i
+#     print(sum)
+
+# 	 Write a program that keeps asking the user to enter numbers  until they enter a negative number. Use a  while  loop. 
+# n=int(input("Enter the number:"))
+# for i in range(0,1):
+#     if(n<0):
+#         print("You Entered a wrong number")
+#     else:
+#         print("Entered Correct Number")
+
+
+
+# i.	Print the first 10 terms of the Fibonacci series using a  for  loop. 
+# n=0
+# m=1
+# for i in range(1,10):
+#     temp=n+m
+#     n=m
+#     m=temp
+#     print(n)
+
+# iii.	Write a program to calculate the factorial of a number using  a  while  loop.  
+# i=1
+# fact=1
+# while i<10:
+#     fact*=i
+#     i+=1
+#     print(fact)
+
+# iv. 	 Print all numbers from 1 to 100 that are divisible by 3 and 5  using a  for  loop. 
+# for i in range(0,100):
+#     if(i%5==0 and i%3==0):
+#         print(i)
+# n=int(input("Enter the number:"))
+# if(n%2==0):
+#     print("its a prime number")
+# else:
+#     print("oddddddddddd")
+                                                                # **pizza**
+# size=(input("Enter the size of the Pizza S/M/L?:"))
+# bill=0
+# if size=="S" or size=="s":
+#     bill+=100
+#     print("the pizza is for 100 rupeess")
+# elif size=="M" or size=="m":
+#     bill+=200
+#     print("the medium pizza is for 200 rupees")
+# else:
+#     bill+=300
+#     print("you choosed large pizza")
+# add_pepperoni=(input("Do you want to add pepperoni y/n?:"))
+# if add_pepperoni=="Y" or add_pepperoni=="y":
+#     if size=="S" or size=="s":
+#         bill+=30
+    
+
+# extra_cheese=input("Do you Want extra cheese y/n")
+# if extra_cheese=="M" or extra_cheese=="m":
+#     bill+=30
+# print(f"your final is {bill}")
+# ***pratice***
+# numbers=[10,20,30,40,50]
+# for i in numbers:
+#     print(i)
+
+# num=0
+# for i in range(1,51):
+#    num+=i
+# print("The sum of the 50 numbers is:",num)
+
+# n=int(input("Enter The Table:"))
+# for i in range(1,21):
+#         print(n,"*",i,"=",n*i)
+
+
+# fact=1
+# n=int(input("Enter The Factorial:"))
+# for i in range(1,n+1):
+#     fact*=i;
+# print("The Fact of ",n,"is",fact)
+
+# rev a number:
+
+# num1 = int(input("Enter a number: "))  
+# count = 0
+# sum_digits = 0
+# rev_num = 0
+
+# while num1 > 0:
+#     r = num1 % 10 
+#     print( r)
+
+#     rev_num = rev_num * 10 + r 
+#     num1 = num1 // 10  
+#     count += 1 
+#     sum_digits += r  
+# print("Reverse :", rev_num)
+# print("Count :", count)
+# print("Sum :", sum_digits)
+
 
 
 
